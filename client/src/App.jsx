00857f59e57c15e9ca492d769106f7985d0eee8f
zakariae <sebaizakariae@gmail.com>
2024-04-09 14:01:33 +0200

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
+import { BroserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="red p-5 bg-slate-600">hello</div>
-    </>
+    <BroserRouter>
+      <Routes>
+        <route path="/" element={<Home />} />
+        <route path="/about" element={<About />} />
+        <route path="/dashboard" element={<Dashboard />} />
+        <route path="/sign-in" element={<Signin />} />
+        <route path="/sign-up" element={<SignUp />} />
+        <route path="/projects" element={<Projects />} />
+      </Routes>
+    </BroserRouter>
   );
 }
 
