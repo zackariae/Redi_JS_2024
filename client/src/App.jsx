9748da2cd08d0b2cf5158fa425b446c3bce5972d
zakariae <sebaizakariae@gmail.com>
2024-04-09 06:14:42 +0200

    install raect and tailwindcss and first templet

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="red p-5 bg-slate-600">hello</div>
+    </>
+  );
+}
+
+export default App;
