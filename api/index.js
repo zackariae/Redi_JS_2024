068acfccd489aeee362186026e03f19d5731753b
zakariae <sebaizakariae@gmail.com>
2024-04-12 05:24:30 +0200

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
+mongoose.
+connect(process.env.MONGODB).
+then(console.log("connected to MongoDb ")).
+catch((error)=>console.log(error));
+
+
 app.listen(3000,()=>{
-    console.log("servHHHHHer Run on port 3000");
+    console.log("server Run on port 3000");
 });
 
