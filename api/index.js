80c286d3b055b2a7bd83cd22e954265ed56a79d0
zakariae <sebaizakariae@gmail.com>
2024-04-15 12:46:12 +0200

    error handler middleware

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 const app = express();
@@ -16,5 +17,14 @@ app.listen(3000,(res,req)=>{
     console.log("server is Running on port 3000");
 });
 
-app.use('/api/user', userRoutes);
+//allow the backend to receive json data
+app.use(express.json());
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth',authRoutes);
+//Error handling middleware
+app.use((err, req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({success: false,statusCode,message});
+});
